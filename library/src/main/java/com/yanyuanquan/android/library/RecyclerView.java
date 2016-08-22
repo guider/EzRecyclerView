d128baf1b0447c37601e317d1fd2d944b647c1b2
guider <guider@yeah.net>
2016-08-22 22:29:49 +0800

    a

---
+++ library/src/main/java/com/yanyuanquan/android/library/RecyclerView.java
@@ -0,0 +1,10 @@
+package com.yanyuanquan.android.library;
+
+/**
+ * Created by guider on 16/8/22.
+ * Email guider@yeah.net
+ * github https://github.com/guider
+ */
+public class RecyclerView  {
+
+}
